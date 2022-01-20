e1018edca989cd929b3214cca1650ff0042993fb
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-01-20 16:31:47 +0100

    update badge css

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { IBadgeProps } from "helpers/interface";
 
 const Badge: React.FC<IBadgeProps> = ({ content }): JSX.Element => (
-	<p className="bagde w-20px h-20px f-row-center-center o-hidden white br-round pointer-none no-select fs-80">
+	<p
+		className={ (content < 100 ? 'badge' : 'badge badge--full') + " w-20px h-20px f-row-center-center o-hidden white br-round pointer-none no-select fs-80" }
+	>
 		{ content >= 100 ? "+99" : content }
 	</p>
 );
